daf5615d30effa8ca3ad9f36b2e2a84df138721b
Andrey Struchkov <struchkovav@yandex.ru>
2018-02-11 14:18:23 +0300

    Вынес функцию

--- functions.php
+++ functions.php
@@ -7,6 +7,17 @@ function include_template($template,$data = []) {
 			return ob_get_clean();
 		}
 	}
+
+// Считаем задачи в проекте
+function count_tasks ($task_list, $category) {
+	$tasks = 0;
+  	if ($category == "Все") {
+    	$tasks = count($task_list); 
+  	} else {
+    	foreach ($task_list as $key => $item) {
+      		if ($item['category'] == $category) $tasks++;
+    	}
+	}
+  	return $tasks;
+}
 ?>
-
-
--- index.php
+++ index.php
@@ -43,18 +43,6 @@ $task_list = [
 	]
     
 ];
-// Считаем задачи в проекте
-function count_tasks ($task_list, $category) {
-	$tasks = 0;
-  	if ($category == "Все") {
-    	$tasks = count($task_list); 
-  	} else {
-    	foreach ($task_list as $key => $item) {
-      		if ($item['category'] == $category) $tasks++;
-    	}
-	}
-  	return $tasks;
-}
 
 require_once('functions.php');
 
