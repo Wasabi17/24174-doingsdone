2c967c7f8d9c5a16b302d290160829323c34d420
Andrey Struchkov <struchkovav@yandex.ru>
2018-02-26 18:16:00 +0300

    Переписал на VALUES

--- queries.sql
+++ queries.sql
@@ -1,19 +1,25 @@
-INSERT INTO users SET email = 'ignat.v@gmail.com', name = 'Игнат', password = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', registration_date = '2018.02.26';
-INSERT INTO users SET email = 'kitty_93@li.ru', name = 'Леночка', password = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', registration_date = '2018.02.26';
-INSERT INTO users SET email = 'warrior07@mail.ru', name = 'Руслан', password = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW', registration_date = '2018.02.26';
+INSERT INTO users(email,name,password,registration_date,contacts) 
+VALUES
+('ignat.v@gmail.com','Игнат','$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka','2018.02.26',null),
+('kitty_93@li.ru','Леночка','$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa','2018.02.26',null),
+('warrior07@mail.ru','Руслан','$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW','2018.02.26',null);
 
-INSERT INTO category SET name = 'Входящие', user_id = 1;
-INSERT INTO category SET name = 'Учеба', user_id = 1;
-INSERT INTO category SET name = 'Работа', user_id = 1;
-INSERT INTO category SET name = 'Домашние дела', user_id = 2;
-INSERT INTO category SET name = 'Авто', user_id = 3;
+INSERT INTO category(name,user_id)
+VALUES
+('Входящие',1),
+('Учеба',1),
+('Работа',1),
+('Домашние дела',2),
+('Авто',3);
 
-INSERT INTO tasks SET name = 'Собеседование в IT-компании', creation_date = '2018.02.10', deadline_date = '2018.06.01', file='uploads/Home.psd', user_id = 1, category_id = 3;
-INSERT INTO tasks SET name = 'Выполнить тестовое задание', creation_date = '2018.02.11', deadline_date = '2018.02.12', user_id = 1, category_id = 3;
-INSERT INTO tasks SET name = 'Сделать задание первого раздела', creation_date = '2018.02.14', done_date = '2018.02.15', deadline_date = '2018.04.21', user_id = 1, category_id = 2;
-INSERT INTO tasks SET name = 'Встреча с другом', creation_date = '2018.02.12', deadline_date = '2018.04.22', user_id = 3, category_id = 1;
-INSERT INTO tasks SET name = 'Купить корм для кота', creation_date = '2018.02.10', user_id = 2, category_id = 4;
-INSERT INTO tasks SET name = 'Заказать пиццу', creation_date = '2018.02.15', file='uploads/Home.psd', user_id = 2, category_id = 4;
+INSERT INTO tasks(name,creation_date,done_date,deadline_date,file,user_id,category_id)
+VALUES
+('Собеседование в IT-компании','2018.02.10',null,'2018.06.01','uploads/Home.psd',1,3),
+('Выполнить тестовое задание','2018.02.11',null,'2018.02.12',null,1,3),
+('Сделать задание первого раздела','2018.02.14','2018.02.15','2018.04.21',null,1,2),
+('Встреча с другом','2018.02.12',null,'2018.04.22',null,3,1),
+('Купить корм для кота','2018.02.10',null,null,null,2,4),
+('Заказать пиццу','2018.02.15',null,null,'uploads/Home.psd',2,4);
 
 /* Получить список из всех проектов для одного пользователя */
 SELECT name FROM category WHERE user_id = 1;
